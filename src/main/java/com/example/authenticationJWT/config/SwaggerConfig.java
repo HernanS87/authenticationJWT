6cfcf010f0380595a6a1bfb65bbda1369be800a8
Hernán Santarelli <hernansantarelli87@gmail.com>
2025-03-01 14:25:38 -0300

    add security config to swagger

--- src/main/java/com/example/authenticationJWT/config/SwaggerConfig.java
+++ src/main/java/com/example/authenticationJWT/config/SwaggerConfig.java
@@ -1,10 +1,15 @@
 package com.example.authenticationJWT.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.http.HttpHeaders;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -31,7 +36,19 @@ import io.swagger.v3.oas.annotations.servers.Server;
                         description = "PROD SERVER",
                         url = "http://sayhello:8080"
                 )
-        }
+        },
+        security = @SecurityRequirement(
+                name = "Security Token"
+        )
+)
+@SecurityScheme(
+        name = "Security Token",
+        description = "Access Token For My API",
+        type = SecuritySchemeType.HTTP,
+        paramName = HttpHeaders.AUTHORIZATION,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
 )
 public class SwaggerConfig {
 }
